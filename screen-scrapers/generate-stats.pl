12bafef56fc13c2267f267ec258d89ccca21c8b6
steve <steve@madlib>
2011-04-11 23:23:44 -0700

    New methodology.

--- screen-scrapers/generate-stats.pl
+++ screen-scrapers/generate-stats.pl
@@ -8,12 +8,16 @@ my $i = 0;
 my $users = 0;
 my $line;
 my %entries;
+my %comments;
 my $min_id = 50000;
 my $max_id = 0;
 
 my $data_file = "data.csv";
+my $comments_file = "comments.txt";
 
 open (DB, ">$data_file") || die ("Can't create file $data_file.");
+open (COMMENTS, ">$comments_file") || die ("Can't create file $comments_file.");
+
 open (FILE, $ARGV[0]) || die ("Can't open file $ARGV[0].");
 
 print DB "name,date,time,comment_id\r\n";
@@ -65,8 +69,24 @@ while ($line = <FILE>)
 					$entries{$name} = $contestant;
 
 					$time =~ s/ 2011 //;
-					$time =~ s/12:00 am/12:01 am/;	# 12:00 am is a special value see below
+					#$time =~ s/12:00 am/12:01 am/;	# 12:00 am is a special value see below
 					print DB "$name,$time,$last_id\r\n";
+					
+					while ($line = <FILE>)
+					{
+						next if ($line !~ /\<div class\=\"box_copy\"\>/);
+						while ($line = <FILE>)
+						{
+							next if ($line !~ /\<p\>(.+)\<\/p\>/);
+							my $comment = $1;
+							if (length $comment < 60 && $comment !~ /\d+/ && $comment !~ /(.)\1\1\1+/)
+							{
+								$comments{$comment} = 1 if ($comment =~ /hipster/i);
+							}
+							last;
+						}
+						last;
+					}
 					last;
 				}
 				last;
@@ -91,16 +111,34 @@ while ($line = <FILE>)
 #	print "\n";
 #}
 
-for (my $n=1;$n < $min_id;$n++)
-{
-	print DB "__unknown__,February 01,12:00 am,$n\r\n"
-}
+
+#for (my $n=1;$n < $min_id;$n++)
+#{
+#	print DB "__unknown__,February 01,12:00 am,$n\r\n"
+#}
 
 my $total = ($min_id - 1) + $i;
 my $missing = $max_id - $total;
-for (my $n=0;$n < $missing;$n++)
+#for (my $n=0;$n < $missing;$n++)
+#{
+#	print DB "__missing__,February 01,12:00 am,$n\r\n"
+#}
+
+my %quotes;
+open(QUOTES, "quotes.txt") || die("fail.");
+while(<QUOTES>)
 {
-	print DB "__missing__,February 01,12:00 am,$n\r\n"
+	if (/(.+)(\.|\?|\!)\s*$/)
+	{
+		my $str = $1 . " hipster" . $2;
+		$quotes{$str} = 1;
+	}
+}
+
+my $key;
+foreach $key (keys %comments)
+{
+	print COMMENTS $key . "\r\n" if not defined($quotes{$key});
 }
 
 print "Count = $i\n";
--- screen-scrapers/live105-scraper.rb
+++ screen-scrapers/live105-scraper.rb
@@ -4,11 +4,9 @@
 
 require 'rubygems'
 require 'mechanize'
-require 'net/smtp'
 require 'logger'
 
 $logger = Logger.new("scraper.log", shift_age = 'daily')
-$failures = 0
 
 def print(x)
   puts x
@@ -17,7 +15,7 @@ end
 
 def init_browser()
   agent = Mechanize.new
-  agent.user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.44 Safari/534.0'
+  agent.user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.44'
   agent.max_history = 1
   agent.open_timeout = 3000
   agent.read_timeout = 3000
@@ -28,75 +26,76 @@ end
 begin
 
   if ARGV[0] == nil
-    print "Error: first arg must be an integer"
+    print "Error: first arg must be a comments file"
     exit
   end
+  comments_file = ARGV[0]
+
+  if ARGV[1] == nil
+    print "Error: second arg must be a names file"
+    exit
+  end
+  names_file = ARGV[1]
+
+  if ARGV[2] == nil
+    print "Error: third arg must be a whales file"
+    exit
+  end
+  whales_file = ARGV[2]
 
   print "Begin mechanize script"
-
-  quotes = []
-
-  print "Loading quotes"
-  unless File.exist?("quotes.txt")
-    print "Error: couldn't find quotes.txt"
-    exit
-  end
-  i = 0
   
-  quotes = IO.readlines("quotes.txt")
+  quotes = IO.readlines(comments_file)
   len = quotes.length
-
-  for c in 0..len
-    if quotes[c] =~ /(.+)(\.|\?|\!)\s*$/
-      quotes[c] = $1 + " hipster" + $2
-    else
-      quotes[c] = quotes[c] + " hipster." unless quotes[c] == nil
-    end
+  for i in 0..len
+    quotes[i] = $1 if quotes[i] =~ /^(.+)$/
   end
-
-  print "Randomizing " + len.to_s + " quotes"
-  for c in 0..len
-    temp = quotes[c]
-    pos = rand(len)
-    quotes[c] = quotes[pos]
-    quotes[pos] = temp
+  
+  names = IO.readlines(names_file)
+  names_len = names.length
+  for i in 0..names_len
+    names[i] = $1 if names[i] =~ /^(.+)$/
   end
-  print "done"
-
+  
+  whales = IO.readlines(whales_file)
+  whales_len = whales.length
+  for i in 0..whales_len
+    whales[i] = $1 if whales[i] =~ /^(\w+)$/
+  end
+  
   agent = init_browser()
   page = agent.get "http://live105.radio.com/2011/02/21/be-comment-40000-with-the-word-hipster-and-win-coachella-tickets"
  
-  base_sleep_amount = 5;
-  rand_amount = 5
+  base_sleep_amount = 7
+  rand_amount = 15
   comment_no = 0
-  i = 0
+  i = 1
 
   while true
-    
-    if i == len
-      print "Randomizing " + len.to_s + " quotes"
-      for c in 0..len
-        temp = quotes[c]
-        pos = rand(len)
-        quotes[c] = quotes[pos]
-        quotes[pos] = temp
-      end
-      print "done"
-    end
 
     print "Attempt number " + i.to_s
     print "Time is " + Time.now.to_s
 
     begin
-      sleep 5
       t1 = Time.now
+      
+      author = whales[rand(whales_len-1)]
+      
+      # all other non-whales post at a lower frequency
+      # at least every 10th post at most every 25th post
+      rand_lowfreq_user = 10 + rand(15)
+      if i % rand_lowfreq_user == 0
+        author = names[rand(names_len-1)]
+        puts "Non-whale author \"" + author + "\" (i=" + i.to_s + ";freq=" + rand_lowfreq_user.to_s + ")"
+      else
+        puts "Whale author \"" + author + "\" (i=" + i.to_s + ")"
+      end
 
       comment_form = page.form_with(:action => 'http://live105.radio.com/wp-comments-post.php')
-      comment_form.field_with(:name => "author").value = "Steve"
-      comment_form.field_with(:name => "email").value = "stoscano@hotmail.com"
-      comment_value = (ARGV[0].to_i+i).to_s + ". " + quotes[i]
-      comment_form.field_with(:name => "comment").value = comment_value
-      print "Submitting comment \"" + comment_value + "\""
+      comment_form.field_with(:name => "author").value = author
+      comment_form.field_with(:name => "email").value = "zotzot209@gmail.com"
+      comment_form.field_with(:name => "comment").value = quotes[i]
+      print "Submitting comment \"" + quotes[i] + "\""
       page = agent.submit comment_form
 
       if page == nil
@@ -125,16 +124,10 @@ begin
       sleep sleep_val.to_i
 
     rescue Exception => e
-      # Retry
-      $failures = $failures + 1
-      # linear backoff
-      sleep_val = 30 * $failures
-      print "Exception caught: \"" + e.to_s + "\", retrying in " + sleep_val.to_s + "..."
+      print "Exception caught: \"" + e.to_s + "\""
       print ""
-      sleep sleep_val.to_i
-      # reestablish connection
-      agent = init_browser()
-      page = agent.get "http://live105.radio.com/2011/02/21/be-comment-40000-with-the-word-hipster-and-win-coachella-tickets"
+      # Outer script should respawn the process
+      exit
     end
   end
 end
